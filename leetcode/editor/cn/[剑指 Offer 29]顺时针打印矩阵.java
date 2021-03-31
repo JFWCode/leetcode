04165692ae03cf98dca388344dfd9075ed40004a
wangke <wangke@iauto.com>
2021-03-31 20:51:45 +0800

    change spiralMatrix code
    Change-Id: Idf7092cdbf057ee1a13c06b8d370e23962330b9a

--- leetcode/editor/cn/[剑指 Offer 29]顺时针打印矩阵.java
+++ leetcode/editor/cn/[剑指 Offer 29]顺时针打印矩阵.java
@@ -36,29 +36,34 @@ class Solution {
         }
 
         int[] arr = new int[matrix.length * matrix[0].length];
-        int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+
+        int left = 0, right = matrix[0].length, top = 0, bottom = matrix.length;
 
         int arrIndex = 0;
-        while (left <= right && top <= bottom) {
-            for (int column = left; column <= right; column++) {
-                arr[arrIndex++] = matrix[top][column];
+        while (left < right && top < bottom) {
+            for (int col = left; col < right; col++) {
+                arr[arrIndex++] = matrix[top][col];
             }
-            for (int row = top + 1; row <= bottom; row++) {
-                arr[arrIndex++] = matrix[row][right];
+            top++;
+
+            for (int row = top; row < bottom; row++) {
+                arr[arrIndex++] = matrix[row][right-1];
             }
-            if (left < right && top < bottom) {
-                for (int column = right - 1; column > left; column--) {
-                    arr[arrIndex++] = matrix[bottom][column];
+            right--;
+
+            if (top < bottom && left < right) {
+                for (int col = right - 1; col >= left; col--) {
+                    arr[arrIndex++] = matrix[bottom-1][col];
                 }
-                for (int row = bottom; row > top; row--) {
+                bottom--;
+
+                for (int row = bottom - 1; row >= top; row--) {
                     arr[arrIndex++] = matrix[row][left];
                 }
+                left++;
             }
-            left++;
-            right--;
-            top++;
-            bottom--;
         }
+
         return arr;
     }
 
